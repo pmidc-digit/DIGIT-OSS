d12aeefa61479e794380f8734be7803ccee3b87f
rakesh-wt-egov <rakesh.katagouni@egovernments.org>
2023-08-08 11:02:33 +0530

    Fixed search issue

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/fsm/useApplicationStatus.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/fsm/useApplicationStatus.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useQuery } from "react-query";
 
-const useApplicationStatus = (select, isEnabled = true, statusMap=[]) => {
+const useApplicationStatus = (select, isEnabled = true, statusMap = []) => {
   const { t } = useTranslation();
 
   const userInfo = Digit.UserService.getUser();
@@ -19,7 +19,7 @@ const useApplicationStatus = (select, isEnabled = true, statusMap=[]) => {
     "CANCELED",
     "COMPLETED",
     "CITIZEN_FEEDBACK_PENDING",
-    "DISPOSAL_IN_PROGRESS"
+    "DISPOSAL_IN_PROGRESS",
   ];
 
   const DSO = Digit.UserService.hasAccess(["FSM_DSO"]);
@@ -27,22 +27,25 @@ const useApplicationStatus = (select, isEnabled = true, statusMap=[]) => {
 
   const tenantId = Digit.ULBService.getCurrentTenantId();
   const fetch = async () => {
-    let WorkflowService = await Digit.WorkflowService.init(tenantId, "FSM,FSM_POST_PAY_SERVICE,PAY_LATER_SERVICE,FSM_ADVANCE_PAY_SERVICE,FSM_ZERO_PAY_SERVICE");
+    let WorkflowService = await Digit.WorkflowService.init(
+      tenantId,
+      "FSM,FSM_POST_PAY_SERVICE,PAY_LATER_SERVICE,FSM_ADVANCE_PAY_SERVICE,FSM_ZERO_PAY_SERVICE"
+    );
     return workflowOrder.map(
       (status) => getStates(WorkflowService.BusinessServices)?.filter((workflowDetails) => status === workflowDetails?.state)[0]
     );
   };
 
   const getStates = (businessServices) => {
-    let states = []
+    let states = [];
     businessServices.map((data) => {
-      states = states.concat(data.states)
-    })
-    return states
-  }
+      states = states.concat(data.states);
+    });
+    return states;
+  };
 
   const roleWiseSelect = (WorkflowService) => {
-    const response = WorkflowService.filter((state) => state.applicationStatus)
+    const response = WorkflowService.filter((state) => state?.applicationStatus)
       .filter((status) => {
         if (status.actions === null) return 0;
         const ref = status.actions.reduce((prev, curr) => [...prev, ...curr.roles], []);
@@ -52,27 +55,27 @@ const useApplicationStatus = (select, isEnabled = true, statusMap=[]) => {
       .map((state) => {
         const roles = state.actions?.map((e) => e.roles)?.flat();
         return {
-          name: t(`CS_COMMON_FSM_${state.applicationStatus}`),
-          code: state.applicationStatus,
-          id: (statusMap?.filter(e => e.applicationstatus === state.applicationStatus)?.[0]?.statusid) || state.uuid,
+          name: t(`CS_COMMON_FSM_${state?.applicationStatus}`),
+          code: state?.applicationStatus,
+          id: statusMap?.filter((e) => e.applicationstatus === state?.applicationStatus)?.[0]?.statusid || state.uuid,
           roles,
         };
-      })
+      });
     return response;
   };
 
   const defaultSelect = (WorkflowService) => {
-    let applicationStatus = WorkflowService.filter((state) => state.applicationStatus).map((state) => {
+    let applicationStatus = WorkflowService.filter((state) => state?.applicationStatus).map((state) => {
       const roles = state.actions?.map((e) => e.roles)?.flat();
       return {
-        name: t(`CS_COMMON_FSM_${state.applicationStatus}`),
-        code: state.applicationStatus,
-        id: (statusMap?.filter(e => e.applicationstatus === state.applicationStatus)?.[0]?.statusid) || state.uuid,
+        name: t(`CS_COMMON_FSM_${state?.applicationStatus}`),
+        code: state?.applicationStatus,
+        id: statusMap?.filter((e) => e?.applicationstatus === state?.applicationStatus)?.[0]?.statusid || state.uuid,
         roles,
       };
     });
 
-    return DSO ? allowedStatusForDSO.map((item) => applicationStatus.filter((status) => status.code === item)[0]) : applicationStatus;
+    return DSO ? allowedStatusForDSO.map((item) => applicationStatus?.filter((status) => status.code === item)[0]) : applicationStatus;
   };
   return useQuery(
     ["APPLICATION_STATUS", isEnabled],
