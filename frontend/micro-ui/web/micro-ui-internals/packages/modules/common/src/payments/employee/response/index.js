a5407bc73c5456b885cdf0a82d4ec060065aa7e0
rakesh-wt-egov <rakesh.katagouni@egovernments.org>
2023-09-14 12:06:18 +0530

    Fixed SM-3049

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/employee/response/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/employee/response/index.js
@@ -65,8 +65,7 @@ export const SuccessfulPayment = (props) => {
   };
 
   const { data: generatePdfKey } = Digit.Hooks.useCommonMDMS(tenantId, "common-masters", "ReceiptKey", {
-    select: (data) =>
-      data["common-masters"]?.uiCommonPay?.filter(({ code }) => businessService?.includes(code))[0]?.receiptKey || "consolidatedreceipt",
+    select: (data) => data["common-masters"]?.uiCommonPay?.filter((items) => businessService === items.code)[0].receiptKey,
   });
 
   const printCertificate = async () => {
@@ -170,9 +169,9 @@ export const SuccessfulPayment = (props) => {
 
   const printDisconnectionRecipet = async () => {
     let tenantid = tenantId ? tenantId : Digit.ULBService.getCurrentTenantId();
-    let consumercode =  window.location.href.substring(window.location.href.lastIndexOf(consumerCode),window.location.href.lastIndexOf("?"));
+    let consumercode = window.location.href.substring(window.location.href.lastIndexOf(consumerCode), window.location.href.lastIndexOf("?"));
     await Digit.Utils.downloadReceipt(consumercode, businessService, "consolidatedreceipt", tenantid);
-  }
+  };
 
   if (businessService?.includes("BPA") && isBpaSearchLoading) return <Loader />;
 
@@ -183,7 +182,11 @@ export const SuccessfulPayment = (props) => {
         <CardText>{getCardText()}</CardText>
         {generatePdfKey ? (
           <div style={{ display: "flex" }}>
-            <div className="primary-label-btn d-grid" style={{ marginLeft: "unset", marginRight: "20px" }} onClick={IsDisconnectionFlow === "true"? printDisconnectionRecipet : printReciept}>
+            <div
+              className="primary-label-btn d-grid"
+              style={{ marginLeft: "unset", marginRight: "20px" }}
+              onClick={IsDisconnectionFlow === "true" ? printDisconnectionRecipet : printReciept}
+            >
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
                 <path d="M0 0h24v24H0z" fill="none" />
                 <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z" />
